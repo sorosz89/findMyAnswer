d8705cb2aa5eecc50360bdfea0b2109ad7872ebe
Orosz Sándor <os.sandor@gmail.com>
2018-10-08 21:11:02 +0200

    async

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const BackGround = require('./config/background');
 const driverSetup = new BackGround();
-let driver = driverSetup.init();
-
 const Operations = require('./operations');
-const Login = require('./ui/po/login')
-let frame = new Operations(driver);
-let login = new Login(driver);
+const Login = require('./ui/po/login');
 
-frame.openContact('orosz.sanyika');
-login.logIn('testUser')
+(async function start() {
+    let driver = await driverSetup.init().catch((e) => console.log('Caught'));
+    let operations = new Operations(driver);   
+    let open = await operations.openContact('orosz.sanyika')
+    return open;
+})();
